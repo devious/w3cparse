0f2da5ac029c0c8178a369227be356fffc8b2d74
Kyle Isom <coder@kyleisom.net>
2012-02-20 21:25:19 +0100

    Initial  commit.

---
+++ src/core.cc
@@ -0,0 +1,17 @@
+/**********************************************************************
+ * file: core.cc                                                      *
+ * author: kyle isom <coder@kyleisom.net>                             *
+ * license: ISC / public domain                                       *
+ *                                                                    *
+ * parse w3c log files.                                               *
+ **********************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "working.\n";
+    return 0;
+}
